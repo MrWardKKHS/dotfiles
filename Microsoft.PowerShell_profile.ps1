09924d4ef756b1f6856ef52a0a452d2646e75307
award <award@kerikerihigh.ac.nz>
2023-06-13 21:41:54 +0100

    profile changes

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
-$classwork = 'C:\Users\award\dev\classwork'
-$beginningPython = 'C:\Users\award\dev\dtc-2023\year-11-beginning-python'
+$classwork = '~\dev\classwork'
+$beginningPython = '~\dev\dtc-2023\year-11-beginning-python'
 function notes {start obsidian://open?vault=Department%20Planning}
 function pull-all-repos {bash ~/dotfiles/scripts/pull-all-repos.sh}
 function push-all-repos {bash ~/dotfiles/scripts/pull-all-repos.sh}
