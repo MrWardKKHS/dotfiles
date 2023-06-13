f1716c4039bd6868a1084a279871ea21f75494f7
MrWardKKHS <award@kerikerihigh.ac.nz>
2023-06-13 13:44:55 +1200

    profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,5 @@
 $classwork = 'C:\Users\award\dev\classwork'
 $beginningPython = 'C:\Users\award\dev\dtc-2023\year-11-beginning-python'
+Function notes {start obsidian://open?vault=Department%20Planning}
 Import-Module posh-git
+
